d9b8f2fed31ef741b880cbcd201ed405502f33c9
Prolifik Lexzy <pro_lifik@live.com>
2018-02-06 13:57:42 +0100

    [Prolifik]

--- LearningCSharp.Cmd/Types/Tester.cs
+++ LearningCSharp.Cmd/Types/Tester.cs
@@ -10,9 +10,14 @@ namespace LearningCSharp.Cmd.Types
     {
         public static void Main()
         {
-            Int32 x; // declaration
-            x = 34;
-            int y = 67; // Initialization
+            Int16 w = 23;
+            short z = w;
+
+            Int32 x = 23;
+            int y = w;
+
+            Int64 a = 89;
+            long b = a;
 
             Console.WriteLine($"{x} + {y} = {x + y}");
         }
