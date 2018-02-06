7d8583e06eb32032baa6b6e951d2f1bc525330e3
Prolifik Lexzy <pro_lifik@live.com>
2018-02-06 12:12:49 +0100

    Initial Commit

---
+++ LearningCSharp.Cmd/Types/Tester.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharp.Cmd.Types
+{
+    public class Tester
+    {
+        public static void Main()
+        {
+        }
+    }
+}
