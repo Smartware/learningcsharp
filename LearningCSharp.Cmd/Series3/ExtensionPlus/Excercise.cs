3aecba44554789319f83035616b8be7b96e25237
Prolifik Lexzy <pro_lifik@live.com>
2018-03-01 10:16:12 +0100

    [Prolifik]

---
+++ LearningCSharp.Cmd/Series3/ExtensionPlus/Excercise.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharp.Cmd.Series3.ExtensionPlus
+{
+    public static class Excercise
+    {
+        public static void Main()
+        {
+
+
+            Console.WriteLine(344095323433434.CommaDelimiter());
+            Console.WriteLine(145345353.CommaDelimiter());
+            Console.WriteLine(567735353.CommaDelimiter());
+            Console.Read();
+        }
+
+        public static string CommaDelimiter(this IComparable value) => string.Format("{0:n3}", value);
+
+        //https://stackoverflow.com/questions/20972845/c-sharp-default-format-for-comma-delimited-numbers
+
+    }
+}
---
+++ LearningCSharp.Cmd/Series3/ExtensionPlus/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Globalization;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharp.Cmd.Series3.ExtensionPlus
+{
+    public static class Program
+    {
+        public static void Main()
+        {
+            //Console.WriteLine("{0:n0}", 100000000);
+            //var myDate = new DateTime(2018, 02, 27);
+
+            var myDate = "2018/02/27";
+            Console.WriteLine(myDate);
+            Console.WriteLine(myDate.ToCyberDateTime());
+
+            Student std = new Student();
+            std.FullName = Console.ReadLine();
+            std.DoB = Console.ReadLine();
+
+            //SaveBioData(std);
+            Console.Read();
+        }
+
+        public static void SaveBioData(Student stud)
+        {
+            if (String.IsNullOrEmpty(stud.FullName))
+            {
+                return;
+            }
+            DateTimeFormatInfo info = DateTimeFormatInfo.InvariantInfo;
+            DateTime dob;
+            if (!DateTime.TryParse(stud.DoB, out dob))
+            {
+                return;
+            }
+
+            SaveToDatabase(stud);
+        }
+
+        private static void SaveToDatabase(Student stud)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static DateTime ToCyberDateTime(this String value)
+        {
+            return DateTime.ParseExact(value, "yyyy/MM/dd", null);
+        }
+    }
+
+    public class Student
+    {
+        public string FullName { get; set; }
+        public String DoB { get; set; }
+    }
+}
