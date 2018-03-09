47ea7282efe89067b27d4eecabc0b9019d58cdf8
Prolifik Lexzy <pro_lifik@live.com>
2018-03-09 13:23:05 +0100

    [Prolifik]

---
+++ LearningCSharp.Cmd/Series5/Tester2.cs
@@ -0,0 +1,41 @@
+﻿using LearningCSharp.Topics.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharp.Cmd.Series5
+{
+    public class Tester2
+    {
+        public static void Main()
+        {
+            UniversityDbContext context = new UniversityDbContext();
+            for (int i = 0; i < 9; i++)
+            {
+                context.Students.Add(new Student() { LastName = $"Great gaby {i}", FirstName = "Law" });
+            }
+            context.SaveChanges();
+
+            var searchItems = context.Students.Where(s => s.LastName != "Great gaby"); ;
+
+            foreach (var removeItem in searchItems)
+            {
+                context.Students.Remove(removeItem);
+            }
+
+
+            context.SaveChanges();
+
+            var students = context.Students.Select(s => new { L = s.LastName, F = s.FirstName }).ToList();
+
+            foreach (var item in students)
+            {
+                Console.WriteLine(item.L);
+            }
+
+            Console.Read();
+        }
+    }
+}
---
+++ LearningCSharp.Topics/EF/UniversityDbContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LearningCSharp.Topics.EF
+{
+    public class UniversityDbContext : DbContext
+    {
+        public UniversityDbContext(): base("UniversityDbContext")
+        {
+        }
+        public DbSet<Student> Students { get; set; }
+    }
+
+    public class Student
+    {
+        public int Id { get; set; }
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+    }
+}
